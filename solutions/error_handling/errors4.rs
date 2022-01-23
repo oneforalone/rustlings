61998813e0d9cee0a51a47249ec61ea5dadad550
Yuqi Liu <yuqi.lyle@outlook.com>
2022-01-23 14:58:14 +0800

    finish execise

---
+++ solutions/error_handling/errors4.rs
@@ -0,0 +1,42 @@
+// errors4.rs
+// Make this test pass! Execute `rustlings hint errors4` for hints :)
+
+
+#[derive(PartialEq, Debug)]
+struct PositiveNonzeroInteger(u64);
+
+#[derive(PartialEq, Debug)]
+enum CreationError {
+    Negative,
+    Zero,
+}
+
+impl PositiveNonzeroInteger {
+    fn new(value: i64) -> Result<PositiveNonzeroInteger, CreationError> {
+        // If-else version
+        // if value == 0 {
+        //     return Err(CreationError::Zero)
+        // } else if value > 0 {
+        //     Ok(PositiveNonzeroInteger(value as u64))
+        // } else {
+        //     return Err(CreationError::Negative)
+        // }
+
+        // match version
+        match value {
+            x if x == 0 => Err(CreationError::Zero),
+            x if x < 0 => Err(CreationError::Negative),
+            x => Ok(PositiveNonzeroInteger(value as u64)),
+        }
+    }
+}
+
+#[test]
+fn test_creation() {
+    assert!(PositiveNonzeroInteger::new(10).is_ok());
+    assert_eq!(
+        Err(CreationError::Negative),
+        PositiveNonzeroInteger::new(-10)
+    );
+    assert_eq!(Err(CreationError::Zero), PositiveNonzeroInteger::new(0));
+}
